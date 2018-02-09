04a9ba28107e355b72b53d991c7055863766b674
votti <vito.zanotelli@gmail.com>
2018-02-09 12:18:45 +0000

    adapted temporary links to dropbox

--- scripts/retrieve_data.R
+++ scripts/retrieve_data.R
@@ -9,10 +9,11 @@
 #              'https://data.mendeley.com/datasets/v58yj49pfr/draft/files/3ef240f2-96ab-4dd7-aba4-68150c286990/Figure_4.zip?dl=1',
 #              'https://data.mendeley.com/datasets/v58yj49pfr/draft/files/8ac64093-0273-4ae3-a53e-fdb3348ee7ac/Figure_S5.zip?dl=1')
 
-urls_data = c('file:///mnt/bbvolume/server_homes/vitoz/Git/cyTOFcompensation/data_b/Figure_1.zip',
-              'file:///mnt/bbvolume/server_homes/vitoz/Git/cyTOFcompensation/data_b/Figure_2-3.zip',
-              'file:///mnt/bbvolume/server_homes/vitoz/Git/cyTOFcompensation/data_b/Figure_4.zip',
-              'file:///mnt/bbvolume/server_homes/vitoz/Git/cyTOFcompensation/data_b/Figure_S5.zip')
+urls_data = c('https://www.dropbox.com/s/hewrzb6ntu35ycu/Figure_1.zip?dl=0',
+              'https://www.dropbox.com/s/gor1w8pi0cwx08z/Figure_2-3.zip?dl=0',
+              'https://www.dropbox.com/s/tzbn981ga02480q/Figure_4.zip?dl=0p',
+              'https://www.dropbox.com/s/qb2dsjd87yz9er6/Figure_S5.zip?dl=0',
+              'https://www.dropbox.com/s/3msjm3g07b905rm/Figure_S3.zip?dl=0')
 
 # set working directory to current script location
 scriptdir <- dirname(parent.frame(2)$ofile)
