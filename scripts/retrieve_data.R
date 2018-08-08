9867159e8cf4d975150029e0b5b60e25e6da1275
votti <vito.zanotelli@gmail.com>
2018-08-08 08:53:20 +0000

    Adapts data paths to final mendely data links

--- scripts/retrieve_data.R
+++ scripts/retrieve_data.R
@@ -9,11 +9,11 @@
 #              'https://data.mendeley.com/datasets/v58yj49pfr/draft/files/3ef240f2-96ab-4dd7-aba4-68150c286990/Figure_4.zip?dl=1',
 #              'https://data.mendeley.com/datasets/v58yj49pfr/draft/files/8ac64093-0273-4ae3-a53e-fdb3348ee7ac/Figure_S5.zip?dl=1')
 
-urls_data = c('https://www.dropbox.com/s/hewrzb6ntu35ycu/Figure_1.zip?dl=1',
-              'https://www.dropbox.com/s/gor1w8pi0cwx08z/Figure_2-3.zip?dl=1',
-              'https://www.dropbox.com/s/tzbn981ga02480q/Figure_4.zip?dl=1',
-              'https://www.dropbox.com/s/qb2dsjd87yz9er6/Figure_S5.zip?dl=1',
-              'https://www.dropbox.com/s/3msjm3g07b905rm/Figure_S3.zip?dl=1')
+urls_data = c('https://data.mendeley.com/datasets/v58yj49pfr/1/files/4cd060d1-9714-4040-ad24-9aa48b47d520/Figure_1.zip?dl=1',
+              'https://data.mendeley.com/datasets/v58yj49pfr/1/files/eeefe10c-49f8-4dc6-84b5-0fd82c7d9449/Figure_2-3.zip?dl=1',
+              'https://data.mendeley.com/datasets/v58yj49pfr/1/files/75c2eea3-0bca-4c7d-a06a-c0a75ebebcd2/Figure_4.zip?dl=1',
+              'https://data.mendeley.com/datasets/v58yj49pfr/1/files/859158e7-2786-481e-b070-99aab43aba36/Figure_S3.zip?dl=1',
+              'https://data.mendeley.com/datasets/v58yj49pfr/1/files/0e52746f-545c-4607-91c2-cd8635d4d269/Figure_S5.zip?dl=1')
 
 # set working directory to current script location
 scriptdir <- dirname(parent.frame(2)$ofile)
